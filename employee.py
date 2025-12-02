81c6ad906f09e8c7eca514f033b46456210b079b
prajwal-911 <prajwalawati101@gmail.com>
2025-12-02 09:10:15 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,13 +1,15 @@
+import pytest 
 def employee_details(name, emp_id, department, salary):
-    result = (
-        f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
-        f"Department: {department}\n"
+    return (
+        f"Employee Name : {name} \n"
+        f"Employee ID: {emp_id} \n"
+        f"Department : {department} \n"
         f"Salary: {salary}"
     )
+
     return result
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     # Sample input (you can change)
     name = "Alice"
     emp_id = "E1001"
